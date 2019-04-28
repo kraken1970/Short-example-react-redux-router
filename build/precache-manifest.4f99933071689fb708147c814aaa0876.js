d102cc0150d8526172877d507d45152afb8e3556
Serg Minin <everestdon@ukr.net>
2019-04-28 17:54:23 +0300

    added build and deploy to heroku

---
+++ build/precache-manifest.4f99933071689fb708147c814aaa0876.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19cca485d9f5d8190273aa0c054c834",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0034d78366e754bf4dc0",
+    "url": "/static/css/main.333e72e1.chunk.css"
+  },
+  {
+    "revision": "e121bcfa947dabd8a63f",
+    "url": "/static/js/2.2d09f30e.chunk.js"
+  },
+  {
+    "revision": "0034d78366e754bf4dc0",
+    "url": "/static/js/main.638ac7c2.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
